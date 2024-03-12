ddb6aee56c9b91fcc9ccc7628e774ed0c0b74eb8
SivMarit <155962331+SivMarit@users.noreply.github.com>
2024-03-12 18:12:58 +0100

    d

--- validateSudokoboard.js
+++ validateSudokoboard.js
@@ -1,28 +1,34 @@
 function validateSudokuboard(sudoboardString) {
 
 
-    //check if length is 16
-    if (sudoboardString.length !== 16) return;
+    //ugyldig brett, feil lengde DONE
+    if (sudoboardString.length !== 16) return 'ugyldig brett, feil lengde';
 
-    //check string contains no letters
+    //ugyldig brett, ugyldig tegn
+    if (!'12 34') return 'ugyldig brett, ugyldig tegn';
 
-    if (!1234567890.test(sudoboardString)) {
-        return;
+    //delvis utfylt, ingen feil DONE
+    if (!sudoboardString.includes('123434122143321')) return 'delvis utfylt, ingen feil';
+
+    //delvis utfylt, samme tall to ganger på en rad DONE
+    if (!sudoboardString.includes(1 - 4 * 2)) return 'delvis utfylt, feil i rad';
+
+    //delvis utfylt, samme tall to ganger på en kolonne
+    'delvis utfylt, feil i kolonne';
+
+    //delvis utfylt, samme tall to ganger i en firkant
+    'delvis utfylt, feil i firkant';
+
+    //helt utfylt, ingen feil
+    if (!sudoboardString.includes(' ')||('123434122143321')) return 'helt utfylt, ingen feil';
+
+    //helt utfylt, samme tall to ganger på en rad'
+    for (let i = 0; i < 4; i++) {
+        if (!validateSudokuboard(sudoboardString)) return 'helt utfylt, feil i rad';
     }
-
-    //check if it's partially filled with no errors
-
-    //Assign new row to a new substring
-
-    //Assign each column to a new substring
-
-    //Assign each box to new substrings
-
-    //check if partially filled an no errors
-
-    //check if rows are valid
-
-    //check if columns are valid
-
-    // return 'helt utfylt, ingen feil';
+    //helt utfylt, samme tall to ganger på en kolonne
+    if (sudoboardString(column[1, 2, 3, 4])) return 'delvis utfylt, feil i kolonne';
 }
+
+// hvis ingen feil
+// return 'helt utfylt, ingen feil';
