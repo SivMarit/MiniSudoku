d2b2d01020a93cae4a88f01a4cfe53fc7efdc860
SivMarit <155962331+SivMarit@users.noreply.github.com>
2024-03-12 16:14:40 +0100

    GYJ

--- validateSudokoboard.js
+++ validateSudokoboard.js
@@ -1,3 +1,28 @@
-function validateSudokuboard(sudoboardString){
-    return 'helt utfylt, ingen feil';
+function validateSudokuboard(sudoboardString) {
+
+
+    //check if length is 16
+    if (sudoboardString.length !== 16) return;
+
+    //check string contains no letters
+
+    if (!1234567890.test(sudoboardString)) {
+        return;
+    }
+
+    //check if it's partially filled with no errors
+
+    //Assign new row to a new substring
+
+    //Assign each column to a new substring
+
+    //Assign each box to new substrings
+
+    //check if partially filled an no errors
+
+    //check if rows are valid
+
+    //check if columns are valid
+
+    // return 'helt utfylt, ingen feil';
 }
