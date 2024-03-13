cd34e005ab671bd8f87e55587423b62d0a6aa88c
SivMarit <155962331+SivMarit@users.noreply.github.com>
2024-03-13 14:52:44 +0100

    d

--- validateSudokoboard.js
+++ validateSudokoboard.js
@@ -1,12 +1,12 @@
 function validateSudokuboard(sudoboardString) {
 
-    // Check length is 16
+    // Check if length is 16 //
     if (sudoboardString.length !== 16) return 'ugyldig brett, feil lengde';
 
-    // Check string contains no letters
-    if (/[a-zA-Z]/.test(sudoboardString)) return 'ugyldig brett, ugyldig tegn';
+    // Check if string contains no letters // exec=execute
+    if (/[a-zA-Z]/.exec(sudoboardString)) return 'ugyldig brett, ugyldig tegn';
 
-    // Check if completely filled
+    // Check if board is completely filled //
     let isFilled = !sudoboardString.includes(" ")
 
     let rows = getRows(sudoboardString)
@@ -22,37 +22,40 @@ function validateSudokuboard(sudoboardString) {
         allValid = true;
     }
 
-    console.log(boxesValid + columnsValid + rowsValid)
-
-    // Check if Partially filled and no errors
+    // Check if Partially filled and no errors //
     if (!isFilled && allValid) return 'delvis utfylt, ingen feil';
 
-    // Check if rows are valid & partially filled
+    // Check if rows are valid & partially filled //
     if (!isFilled && !rowsValid) return 'delvis utfylt, feil i rad';
 
-    // Check if columns are valid & partially filled
+    // Check if columns are valid & partially filled //
     if (!isFilled && !columnsValid) return 'delvis utfylt, feil i kolonne';
 
-    // Check if boxes are valid
+    // Check if boxes are valid //
     if (!isFilled && !boxesValid) return 'delvis utfylt, feil i firkant';
 
-    // Check if completely filled and two numbers in row
+    // Check if completely filled and two numbers in row //
     if (isFilled && !rowsValid) return 'helt utfylt, feil i rad';
 
-    // Check if completely filled and two numbers in column
+    // Check if completely filled and two numbers in column //
     if (isFilled && !columnsValid) return 'helt utfylt, feil i kolonne';
 
-    // Completely filled and no errors
+    // Completely filled and no errors //
     return 'helt utfylt, ingen feil';
 }
 
-// A function for checking if a given string has repeated characters
-function hasRepeats(str) {
-    return /(.).*\1/.test(str.replaceAll(" ", ""));
+// A function for checking if a given string has repeated characters //
+function hasRepeats(string) {
+
+    return /(.).*\1/.test(string.replaceAll(" ", "")); //ignorerer mellomrom som et tegn
 }
 
+// function hasRepeats(str) {
+//    const chars =new Set(str.replace(/\s/g, ''));
+//    return chars.size !==str.replace(/\s/g, '').length;
+// }
 function getRows(sudokuString) {
-    // Assign each row to a new substring
+    // Assign each row to a new substring //
     let one = sudokuString.charAt(0) + sudokuString.charAt(1) + sudokuString.charAt(2) + sudokuString.charAt(3);
     let two = sudokuString.charAt(4) + sudokuString.charAt(5) + sudokuString.charAt(6) + sudokuString.charAt(7);
     let three = sudokuString.charAt(8) + sudokuString.charAt(9) + sudokuString.charAt(10) + sudokuString.charAt(11);
@@ -62,7 +65,7 @@ function getRows(sudokuString) {
 }
 
 function getColumns(sudokuString) {
-    // Assign each column to a new substring
+    // Assign each column to a new substring //
     let one = sudokuString.charAt(0) + sudokuString.charAt(4) + sudokuString.charAt(8) + sudokuString.charAt(12);
     let two = sudokuString.charAt(1) + sudokuString.charAt(5) + sudokuString.charAt(9) + sudokuString.charAt(13);
     let three = sudokuString.charAt(2) + sudokuString.charAt(6) + sudokuString.charAt(10) + sudokuString.charAt(14);
@@ -72,12 +75,12 @@ function getColumns(sudokuString) {
 }
 
 function getBoxes(sudokuString) {
-    // Assign each box to new substrings
+    // Assign each box to new substrings //
     let one = sudokuString.charAt(0) + sudokuString.charAt(1) + sudokuString.charAt(4) + sudokuString.charAt(5);
     let two = sudokuString.charAt(2) + sudokuString.charAt(3) + sudokuString.charAt(6) + sudokuString.charAt(7);
     let three = sudokuString.charAt(8) + sudokuString.charAt(9) + sudokuString.charAt(12) + sudokuString.charAt(13);
     let four = sudokuString.charAt(10) + sudokuString.charAt(11) + sudokuString.charAt(14) + sudokuString.charAt(15);
-    
+
     return [one, two, three, four]
 }
 
