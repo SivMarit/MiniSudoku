55125ed5e509f4285dd538b45cb1b67a97db0237
SivMarit <155962331+SivMarit@users.noreply.github.com>
2024-03-17 15:52:52 +0100

    d

--- validateSudokoboard.js
+++ validateSudokoboard.js
@@ -1,12 +1,12 @@
 function validateSudokuboard(sudoboardString) {
 
-    // Check if length is 16 //
+    // Checks if length is 16 //
     if (sudoboardString.length !== 16) return 'ugyldig brett, feil lengde';
 
-    // Check if string contains no letters // exec=execute
+    // Checks if string contains no letters // exec=execute
     if (/[a-zA-Z]/.exec(sudoboardString)) return 'ugyldig brett, ugyldig tegn';
 
-    // Check if board is completely filled //
+    // Checks if board is completely filled //
     let isFilled = !sudoboardString.includes(" ")
 
     let rows = getRows(sudoboardString)
@@ -22,32 +22,32 @@ function validateSudokuboard(sudoboardString) {
         allValid = true;
     }
 
-    // Check if Partially filled and no errors //
+    // Checks if partially filled and no errors //
     if (!isFilled && allValid) return 'delvis utfylt, ingen feil';
 
-    // Check if rows are valid & partially filled //
+    // Checks if rows are valid & partially filled //
     if (!isFilled && !rowsValid) return 'delvis utfylt, feil i rad';
 
-    // Check if columns are valid & partially filled //
+    // Checks if columns are valid & partially filled //
     if (!isFilled && !columnsValid) return 'delvis utfylt, feil i kolonne';
 
-    // Check if boxes are valid //
+    // Checks if boxes are valid //
     if (!isFilled && !boxesValid) return 'delvis utfylt, feil i firkant';
 
-    // Check if completely filled and two numbers in row //
+    // Checks if completely filled and two numbers in row //
     if (isFilled && !rowsValid) return 'helt utfylt, feil i rad';
 
-    // Check if completely filled and two numbers in column //
+    // Checks if completely filled and two numbers in column //
     if (isFilled && !columnsValid) return 'helt utfylt, feil i kolonne';
 
-    // Completely filled and no errors //
+    // Checks completely filled and no errors //
     return 'helt utfylt, ingen feil';
 }
 
 // A function for checking if a given string has repeated characters //
 function hasRepeats(string) {
 
-    return /(.).*\1/.test(string.replaceAll(" ", "")); //ignorerer mellomrom som et tegn
+    return /(.).*\1/.test(string.replaceAll(" ", "")); //ignores spaces as characters
 }
 
 // function hasRepeats(str) {
@@ -55,7 +55,7 @@ function hasRepeats(string) {
 //    return chars.size !==str.replace(/\s/g, '').length;
 // }
 function getRows(sudokuString) {
-    // Assign each row to a new substring //
+    // Assigns each row to a new substring //
     let one = sudokuString.charAt(0) + sudokuString.charAt(1) + sudokuString.charAt(2) + sudokuString.charAt(3);
     let two = sudokuString.charAt(4) + sudokuString.charAt(5) + sudokuString.charAt(6) + sudokuString.charAt(7);
     let three = sudokuString.charAt(8) + sudokuString.charAt(9) + sudokuString.charAt(10) + sudokuString.charAt(11);
@@ -65,7 +65,7 @@ function getRows(sudokuString) {
 }
 
 function getColumns(sudokuString) {
-    // Assign each column to a new substring //
+    // Assigns each column to a new substring //
     let one = sudokuString.charAt(0) + sudokuString.charAt(4) + sudokuString.charAt(8) + sudokuString.charAt(12);
     let two = sudokuString.charAt(1) + sudokuString.charAt(5) + sudokuString.charAt(9) + sudokuString.charAt(13);
     let three = sudokuString.charAt(2) + sudokuString.charAt(6) + sudokuString.charAt(10) + sudokuString.charAt(14);
@@ -75,7 +75,7 @@ function getColumns(sudokuString) {
 }
 
 function getBoxes(sudokuString) {
-    // Assign each box to new substrings //
+    // Assigns each box to new substrings //
     let one = sudokuString.charAt(0) + sudokuString.charAt(1) + sudokuString.charAt(4) + sudokuString.charAt(5);
     let two = sudokuString.charAt(2) + sudokuString.charAt(3) + sudokuString.charAt(6) + sudokuString.charAt(7);
     let three = sudokuString.charAt(8) + sudokuString.charAt(9) + sudokuString.charAt(12) + sudokuString.charAt(13);
